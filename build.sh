b93f97d3d5c74e4a63c51c89d8079ac3cb207d8f
Jere Alho <alho.jere@gmail.com>
2025-01-01 21:20:57 +0200

    Init

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xe
+
+BUILDER_IMAGE="docker.io/texlive/texlive@sha256:7e91f1cc80c471707291548282d1f87ea01c5d21ef525a4c7173b3c6a9b52efa"
+BUILDER_CONTAINER="temp-tex-builder"
+
+podman container run --rm --name $BUILDER_CONTAINER -d $BUILDER_IMAGE sleep 1m
+
+podman cp src/main.tex $BUILDER_CONTAINER:/workdir/main.tex
+podman cp src/references.bib $BUILDER_CONTAINER:/workdir/references.bib
+
+podman exec $BUILDER_CONTAINER sh -c "latexmk -pdf /workdir/main.tex"
+podman cp $BUILDER_CONTAINER:/workdir/main.pdf ./main.pdf
+
+podman container kill $BUILDER_CONTAINER
